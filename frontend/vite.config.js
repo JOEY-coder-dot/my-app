e8d6d411eb0c103ff0c1cf8d2ba7cc0d2cf2deda
JOEY-coder-dot <joeyricalde66@gmail.com>
2026-04-12 18:00:34 +0800

    Successful build with code splitting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000, // 2 MB
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react";
+            if (id.includes("@mui")) return "mui";
+            if (id.includes("recharts")) return "charts";
+            if (id.includes("jspdf") || id.includes("html2canvas")) return "pdf";
+          }
+        },
+      },
+    },
+  },
+});
